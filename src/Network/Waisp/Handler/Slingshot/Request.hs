83b10e342c6e4d814f4d057677c19b9000323704
Danny Navarro <j@dannynavarro.net>
2014-04-02 12:07:45 +0200

    Implement `requestLineParser`

--- src/Network/Waisp/Handler/Slingshot/Request.hs
+++ src/Network/Waisp/Handler/Slingshot/Request.hs
@@ -1,15 +1,17 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Network.Waisp.Handler.Slingshot.Request where
 
-import Control.Applicative ((<$>), (<*>))
+import Control.Applicative -- ((<$), (<$>), (<*>), (<|>))
 import Data.Word (Word8)
-import Data.Attoparsec (Parser)
+import Data.ByteString (ByteString)
+import Data.Attoparsec.Char8
+  -- (Parser, stringCI)
 import Network.Waisp
-  ( RequestMessageHeader(..)
-  , RequestLine
-  , RequestHeaders
-  , Host
-  )
+  -- ( RequestMessageHeader(..)
+  -- , RequestLine
+  -- , RequestHeaders
+  -- , Host
+  -- )
 
 -- * Attoparsec
 
@@ -19,8 +21,39 @@ requestMessageHeaderParser =
                          <*> hostParser
                          <*> requestHeadersParser
 
+-- ** Request Line
+
 requestLineParser :: Parser RequestLine
-requestLineParser = undefined
+requestLineParser =
+    RequestLine <$> methodParser <* char8 ' '
+                <*> pathInfoParser
+                <*> try queryParser
+                <*> httpVersionParser
+
+methodParser :: Parser Method
+methodParser = GET     <$ stringCI "get"
+           <|> PUT     <$ stringCI "put"
+           <|> POST    <$ stringCI "post"
+           <|> HEAD    <$ stringCI "head"
+           <|> DELETE  <$ stringCI "delete"
+           <|> TRACE   <$ stringCI "trace"
+           <|> CONNECT <$ stringCI "connect"
+           <|> OPTIONS <$ stringCI "options"
+
+pathInfoParser :: Parser PathInfo
+pathInfoParser = takeWhile1 (\c -> c /= ' ' || c /= '?')
+
+queryParser :: Parser Query
+queryParser = char '?' *> takeWhile1 isSpace
+
+httpVersionParser :: Parser HttpVersion
+httpVersionParser = HttpVersion
+                <$  stringCI "http/"
+                <*> decimal
+                <*  char '.'
+                <*> decimal
+
+-- * Headers
 
 hostParser :: Parser Host
 hostParser = undefined
@@ -28,7 +61,10 @@ hostParser = undefined
 requestHeadersParser :: Parser RequestHeaders
 requestHeadersParser = undefined
 
--- * Helpers
+-- * Common helpers
 
 crlf :: Parser Word8
 crlf = undefined
+
+-- isToken :: Word8 -> Bool
+-- isToken w = w <= 127 && notInClass "\0-\31()<>@,;:\\\"/[]?={} \t" w

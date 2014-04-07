b08f79bb46844c34ebd3adf4a0d33b4c4e971c91
Danny Navarro <j@dannynavarro.net>
2014-04-07 16:16:29 +0200

    Skeleton for `serveSettingsSocket`

--- src/Network/Waisp/Handler/Slingshot.hs
+++ src/Network/Waisp/Handler/Slingshot.hs
@@ -1,10 +1,14 @@
 module Network.Waisp.Handler.Slingshot where
 
+import Control.Monad (void, forever)
 import Network.Socket (Socket, close)
 import Control.Monad.Catch (bracket)
+import Pipes ((>->), runEffect)
+import Pipes.Network.TCP (acceptFork, fromSocket, toSocket)
 import Network.Waisp (Application)
 import Network.Waisp.Handler.Slingshot.Settings
 import Network.Waisp.Handler.Slingshot.Network
+
 
 type Port = Int
 
@@ -15,7 +19,12 @@ serveSettings :: Settings -> Application -> IO r
 serveSettings set app = bracket
     (bindPortTCP (settingsPort set) (settingsHost set))
      close
-    (\socket -> serveSettingsSocket set socket app)
+    (\sock -> serveSettingsSocket set sock app)
 
+-- TODO: use `pipes-safe` variant
 serveSettingsSocket :: Settings -> Socket -> Application -> IO r
-serveSettingsSocket = undefined
+serveSettingsSocket set sock app =
+    forever . void . acceptFork sock $ \(sock', _) ->
+        runEffect $ fromSocket sock' 4096
+                >-> undefined
+                >-> toSocket sock'

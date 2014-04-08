55de08735089a52c8149db17e1675e6c9e2c7ae2
Danny Navarro <j@dannynavarro.net>
2014-04-08 15:28:44 +0200

    Naive implementation of `serveSettingsSocket`

--- src/Network/Waisp/Handler/Slingshot.hs
+++ src/Network/Waisp/Handler/Slingshot.hs
@@ -1,14 +1,19 @@
 module Network.Waisp.Handler.Slingshot where
 
 import Control.Monad (void, forever)
+import Data.ByteString (ByteString)
 import Network.Socket (Socket, close)
-import Control.Monad.Catch (bracket)
-import Pipes ((>->), runEffect)
-import Pipes.Network.TCP (acceptFork, fromSocket, toSocket)
-import Network.Waisp (Application)
-import Network.Waisp.Handler.Slingshot.Settings
-import Network.Waisp.Handler.Slingshot.Network
+import Control.Monad.Trans.State.Strict (runStateT)
 
+import Control.Monad.Catch (bracket)
+import Pipes (Producer, (>->), runEffect)
+import Pipes.Network.TCP (acceptFork, fromSocket, toSocket)
+import Pipes.Attoparsec (parse)
+
+import Network.Waisp (Application(..), Request(..), Response(..))
+import Network.Waisp.Handler.Slingshot.Settings
+import Network.Waisp.Handler.Slingshot.Request
+import Network.Waisp.Handler.Slingshot.Network
 
 type Port = Int
 
@@ -23,8 +28,27 @@ serveSettings set app = bracket
 
 -- TODO: use `pipes-safe` variant
 serveSettingsSocket :: Settings -> Socket -> Application -> IO r
-serveSettingsSocket set sock app =
-    forever . void . acceptFork sock $ \(sock', _) ->
-        runEffect $ fromSocket sock' 4096
-                >-> undefined
-                >-> toSocket sock'
+serveSettingsSocket _ sock (Application f) =
+    forever . void . acceptFork sock $ \(sock', _) -> do
+        mreq <- recvRequest sock'
+        case mreq of
+             Just req -> f req >>= sendResponse sock'
+             Nothing -> return ()
+
+recvRequest :: Socket -> IO (Maybe Request)
+recvRequest sock = do
+    (mh, body) <- runStateT (parse requestMessageHeaderParser) (fromSocket sock 4096)
+    case mh of
+         Nothing -> return Nothing
+         Just eh  -> case eh of
+                          Left _  -> return Nothing
+                          Right h -> return . Just $ Request h body
+
+sendResponse :: Socket -> Response -> IO ()
+sendResponse sock res = runEffect $ serialize res >-> toSocket sock
+
+class Serializable a where
+    serialize :: Monad m => a -> Producer ByteString m r
+
+instance Serializable Response where
+    serialize = undefined
